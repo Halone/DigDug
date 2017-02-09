a399b07a7a5173925b2f7207be5678dc2da69bb5
Nicolas Touzain <n.touzain@student.isartdigital.com>
2017-02-09 16:18:36 +0100

    first real commit

---
+++ Assets/BlockDestroyer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class BlockDestroyer: MonoBehaviour {
+    public MeshCreator World;
+
+    void Update() {
+        int l_X = (int)Mathf.Round(gameObject.transform.position.x);
+        int l_Y = (int)Mathf.Round(gameObject.transform.position.y);
+
+        if (World.GetBlockType(l_X, l_Y) != (int)MeshCreator.MAP_TYPE.EMPTY) {
+            World.DestroyBlock(l_X, l_Y);
+        }
+    }
+}
