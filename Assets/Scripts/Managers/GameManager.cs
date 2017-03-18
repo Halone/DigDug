b8fdad1f788dc872166028e6531d5290317c2602
Nicolas Touzain <n.touzain@student.isartdigital.com>
2017-03-18 14:12:40 +0100

    singleton et basemanager

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+
+public class GameManager: Singleton<GameManager> {
+    #region Variables
+    public Action onInit;
+    public Action onMenu;
+    public Action onPlay;
+    public Action onLoose;
+    public Action onWin;
+    #endregion
+
+    #region Initialisation & Destroy
+    protected override IEnumerator CoroutineStart() {
+        //while (MenuManager.instance == null) yield return false;
+        //while (!MenuManager.instance.isReady) yield return false;
+        Init();
+
+        //while (!MenuManager.instance.isInit) yield return false;
+        Menu();
+
+        yield return true;
+    }
+
+    protected override void Destroy() {
+        onInit  = null;
+        onMenu  = null;
+        onPlay  = null;
+        onLoose = null;
+        onWin   = null;
+
+        base.Destroy();
+    }
+    #endregion
+
+    #region Game Events
+    private void Init() {
+        if (onInit != null) onInit();
+    }
+
+    private void Menu() {
+        if (onMenu != null) onMenu();
+    }
+
+    private void Play() {
+        if (onPlay != null) onPlay();
+    }
+
+    private void Loose() {
+        if (onLoose != null) onLoose();
+    }
+
+    private void Win() {
+        if (onWin != null) onWin();
+    }
+    #endregion
+}
