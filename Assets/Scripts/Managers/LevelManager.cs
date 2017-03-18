5fdc7a62bcbee700639550685f31dfefc752c2f3
Nicolas Touzain <n.touzain@student.isartdigital.com>
2017-03-18 14:44:48 +0100

    json object + debut levelManager

---
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager: BaseManager<LevelManager> {
+    #region Variables
+    private enum MAP_TYPE {
+        EMPTY,
+        GRASS,
+        STONE
+    }
+
+    private Dictionary<Vector2, MAP_TYPE> m_Map;
+    #endregion
+
+    #region Initialisation & Destroy
+    protected override IEnumerator CoroutineStart() {
+        yield return true;
+        isReady = true;
+    }
+    #endregion
+
+    #region Level Managment
+    protected override void Play() {
+        GenerateMap();
+    }
+
+    private void GenerateMap() {
+        
+    }
+    #endregion
+}
