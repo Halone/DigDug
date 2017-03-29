0177888402367d210a4db44b9017a1fe166b488f
ErwanClement <erwan.clement@laposte.net>
2017-03-29 17:57:57 +0200

    Up Path

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -79,6 +79,9 @@ public class LevelManager: BaseManager<LevelManager> {
         Camera.main.transform.position  = m_World.bounds.center + Vector3.back * 10;
         Camera.main.orthographicSize    = Mathf.Min(m_MapSizeX, m_MapSizeY) / 2;
         Camera.main.orthographicSize    /= Camera.main.pixelRect.width / Camera.main.pixelRect.height;
+
+        List<Vector2>  l;
+        GetPath(new Vector2(2, 10), out l);
     }
 
     private void UpdateWorld() {
@@ -234,4 +237,87 @@ public class LevelManager: BaseManager<LevelManager> {
     }
     #endregion
     #endregion
+
+    #region Pathfinding
+    public bool GetPath(Vector2 m_Pos, out List<Vector2> m_Path)
+    {
+        Dictionary<Vector2, int> l_PropaMap = new Dictionary<Vector2, int>();
+        List<Vector2> l_List = new List<Vector2>();
+        l_List.Add(m_Pos);
+
+        RecursiveMethode(l_PropaMap, l_List);
+        print(l_PropaMap.Count);
+
+        m_Path = new List<Vector2>();
+        return true;
+    }
+
+    public Vector2 GetModelPos(GameObject m_Obj)
+    {
+        return new Vector2();
+    }
+
+    public Dictionary<Vector2, int> RecursiveMethode(Dictionary<Vector2, int> p_Map, List<Vector2> p_List, int p_Iter = 0)
+    {
+        List<Vector2> l_NextIter = new List<Vector2>();
+        DIRECTION[] l_Directions = (DIRECTION[])Enum.GetValues(typeof(DIRECTION));
+        TILE_TYPE l_Type;
+
+        foreach (Vector2 l_Pos in p_List)
+        {
+            foreach (DIRECTION l_Direction in l_Directions)
+            {
+                Vector2 l_NextPos = GetNextCellPos(l_Pos, l_Direction);
+                if(m_Model.TryGetValue(l_NextPos, out l_Type))
+                {
+                    if(l_Type == TILE_TYPE.EMPTY && !p_Map.ContainsKey(l_NextPos))
+                    {
+                        p_Map.Add(l_NextPos, p_Iter);
+                        l_NextIter.Add(l_NextPos);
+                    }
+                }
+            }
+        }
+
+        if (l_NextIter.Count > 0)
+            return RecursiveMethode(p_Map, l_NextIter, p_Iter++);
+        else
+            return p_Map;
+    }
+
+    #region Utils
+    private Vector2 GetNextCellPos(Vector2 p_CurrentCell, DIRECTION p_Direction)
+    {
+        Vector2 p_NextCellPos = p_CurrentCell;
+        switch (p_Direction)
+        {
+            case DIRECTION.UP:
+                p_NextCellPos.y++;
+                break;
+            case DIRECTION.RIGHT:
+                p_NextCellPos.x++;
+                break;
+            case DIRECTION.BOTTOM:
+                p_NextCellPos.y--;
+                break;
+            case DIRECTION.LEFT:
+                p_NextCellPos.x--;
+                break;
+            default:
+                Debug.LogError("Pathfing2D: Error Direction '" + p_Direction + "' not found");
+                break;
+        }
+        return p_NextCellPos;
+
+    }
+    #endregion
+    #endregion
+}
+
+public enum DIRECTION
+{
+    UP,
+    RIGHT,
+    BOTTOM,
+    LEFT
 }
